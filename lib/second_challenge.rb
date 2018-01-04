116526dbf6685f55c0241fc7c406a26c7fe2ac68
apocalypse575 <apocalypse575@gmail.com>
2018-01-04 15:33:16 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
   }
 
   #code your solution here!
+  groceries.values.flatten
 
-  
+
 
 end
+
+#second_challenge uses the .values method to collect all of the grocery items from the groceries hash
